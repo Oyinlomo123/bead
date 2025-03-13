238c030932a551683ddbbfda65a919e2572abc0e
oyintech <saheedbarakat24@gmail.com>
2025-03-13 11:23:29 +0100

    forth commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './Hero'
-import Features from '../reusable/features'
+import Features from './Features'
 import Product from '../reusable/Product'
 
 const Home = () => {
